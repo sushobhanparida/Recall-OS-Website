922a28e22bad4e9668731ae14ac1bb912cce4bd5
sushobhanparida <sushobhan96@gmail.com>
2026-05-08 23:50:37 +0530

    Initial commit

---
+++ supabase-config.example.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://YOUR-PROJECT-REF.supabase.co',
+  anonKey: 'YOUR-ANON-KEY'
+};
---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://fpqylvknoibmiogorzgv.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwcXlsdmtub2libWlvZ29yemd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNjI1NjMsImV4cCI6MjA5MzgzODU2M30.yISMFGsrpxmOAju41E_ORJpSEMSAalZEcTeGDI2O69M'
+};
